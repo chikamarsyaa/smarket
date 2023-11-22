20d6e135ad39f0648a4b331b805a97fea227fa12
Chika Marsya Diandra <chikamarsyad3@gmail.com>
2023-11-22 11:45:54 +0700

    django regist'

--- authentication/views.py
+++ authentication/views.py
@@ -3,23 +3,7 @@ from django.contrib.auth import authenticate, login as auth_login
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import logout as auth_logout
-
-@csrf_exempt
-def logout(request):
-    username = request.user.username
-
-    try:
-        auth_logout(request)
-        return JsonResponse({
-            "username": username,
-            "status": True,
-            "message": "Logout berhasil!"
-        }, status=200)
-    except:
-        return JsonResponse({
-        "status": False,
-        "message": "Logout gagal."
-        }, status=401)
+from .forms import CustomUserCreationForm
 
 @csrf_exempt
 def login(request):
@@ -47,3 +31,50 @@ def login(request):
             "status": False,
             "message": "Login gagal, periksa kembali email atau kata sandi."
         }, status=401)
+    
+
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logout berhasil!"
+        }, status=200)
+    except:
+        return JsonResponse({
+        "status": False,
+        "message": "Logout gagal."
+        }, status=401)
+    
+
+
+from .forms import CustomUserCreationForm
+
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)  # Otomatis login setelah registrasi
+
+            return JsonResponse({
+                'username': user.username,
+                'status': True,
+                'message': 'Registrasi sukses!',
+            }, status=201)
+        else:
+            return JsonResponse({
+                'status': False,
+                'message': 'Registrasi gagal. Periksa kembali formulir Anda.',
+                'errors': form.errors,
+            }, status=400)
+    else:
+        return JsonResponse({
+            'status': False,
+            'message': 'Metode permintaan tidak valid.',
+        }, status=405)
